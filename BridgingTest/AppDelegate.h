d3b9828c1cdc99fd7bff009bbb91bf8441688bf6
Sudheer Keshav Bhat <sudheer.bhat@robosoftin.com>
2015-11-16 18:38:56 +0530

    Initial Commit

---
+++ BridgingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BridgingTest
+//
+//  Created by Sudheer Keshav Bhat on 16/11/15.
+//  Copyright (c) 2015 Robosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BridgingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BridgingTest
+//
+//  Created by Sudheer Keshav Bhat on 16/11/15.
+//  Copyright (c) 2015 Robosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
